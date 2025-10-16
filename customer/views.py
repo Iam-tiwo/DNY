04a5ddff93d3c36355a8446405fe3698c04984b5
Adebanjo Taiwo <146878677+Iam-tiwo@users.noreply.github.com>
2025-10-16 01:04:23 +0100

    Update views.py

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,4 @@
+
 import json
 import random
 import datetime
@@ -100,6 +101,19 @@ from django.db.models import Sum
 from django.contrib.auth.models import Group
 from .models import Order, Visitor, Customer, Notice
 from .decorators import allowed_users  # adjust if needed
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+# Auto-create admin user if missing (temporary)
+if not User.objects.filter(username="DNY").exists():
+    User.objects.create_superuser(
+        username="DNY",
+        email="tiwoadex@gmail.com",
+        password="Kabiru123"
+    )
+
 
 def get_week_range(year, week):
     """Returns the start (Monday) and end (next Monday) of the ISO week."""
